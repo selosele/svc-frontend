5d2edae19deb4f5b5c83c0bbb6a1996cdc6f5441
selosele <osh3384@naver.com>
2024-06-07 15:47:42 +0900

    로그인 페이지 PATH 변경

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 
+/** 인증 라우터 */
 export const authRoutes: Routes = [
   
   // 로그인 페이지
   {
-    path: 'sign-in',
+    path: 'auth/sign-in',
     loadComponent: () => import('./sign-in/sign-in.component').then(x => x.SignInComponent),
   },
 ];
--- src/app/shared/guards/auth/auth.guard.ts
+++ src/app/shared/guards/auth/auth.guard.ts
@@ -13,7 +13,7 @@ export class AuthGuard {
   canActivate() {
     // 로그인이 안 되어 있으면 로그인 페이지로 이동한다.
     if (!this.authService.isSignIned()) {
-      this.router.navigate(['/sign-in']);
+      this.router.navigateByUrl('/auth/sign-in');
     }
   }
 
