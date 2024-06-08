a57ed6f73d0fbbfa05f04481e4ecc7d90f3678d7
selosele <osh3384@naver.com>
2024-06-08 14:55:48 +0900

    리팩토링

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -28,8 +28,6 @@ export class AuthService {
 
   /** 로그인 여부를 반환한다. */
   isSignIned(): boolean {
-
-    /** 액세스 토큰 */
     const accessToken = window.localStorage.getItem('accessToken');
     if (isNotBlank(accessToken)) // TODO: 토큰 유효성 검증도 필요
       return true;
