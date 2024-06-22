674d53107cd41db03e50f939a5753faa637634a9
selosele <osh3384@naver.com>
2024-06-22 16:23:23 +0900

    로그인/로그아웃 시 메뉴조회이슈 수정

--- src/app/shared/components/layout/layout-menu/layout-menu.component.ts
+++ src/app/shared/components/layout/layout-menu/layout-menu.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MenuService } from '@app/shared/services';
+import { UiLinkComponent } from '../../ui';
+import { AuthService } from '@app/auth/auth.service';
 
 @Component({
   standalone: true,
   imports: [
     CommonModule,
+    UiLinkComponent,
   ],
   selector: 'layout-menu',
   templateUrl: './layout-menu.component.html',
@@ -15,6 +18,7 @@ export class LayoutMenuComponent implements OnInit {
 
   constructor(
     private menuService: MenuService,
+    private authService: AuthService,
   ) {}
 
   /** 메뉴 목록 */
@@ -22,7 +26,7 @@ export class LayoutMenuComponent implements OnInit {
 
   ngOnInit(): void {
     this.menuList$.subscribe((menuList) => {
-      if (menuList.length === 0) {
+      if (menuList.length === 0 && this.authService.isLogined()) {
         this.listMenu();
       }
     });
