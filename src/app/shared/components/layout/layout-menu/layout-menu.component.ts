99c8865e242128da20785f009be70c599c6c32ba
selosele <osh3384@naver.com>
2024-06-23 14:17:20 +0900

    메뉴 스타일 개선

--- src/app/shared/components/layout/layout-menu/layout-menu.component.ts
+++ src/app/shared/components/layout/layout-menu/layout-menu.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { MenuService } from '@app/shared/services';
-import { UiLinkComponent } from '../../ui';
 import { AuthService } from '@app/auth/auth.service';
+import { UiLinkComponent } from '../../ui';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -12,17 +13,31 @@ import { AuthService } from '@app/auth/auth.service';
   ],
   selector: 'layout-menu',
   templateUrl: './layout-menu.component.html',
-  styleUrl: './layout-menu.component.scss'
+  styleUrl: './layout-menu.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class LayoutMenuComponent implements OnInit {
 
   constructor(
     private menuService: MenuService,
     private authService: AuthService,
-  ) {}
+    private router: Router,
+  ) {
+    router.events.subscribe(val => {
+      if (val instanceof NavigationEnd) {
+        this.activeMenuUrl = val.url;
+      }
+    });
+  }
 
   /** 메뉴 목록 */
   menuList$ = this.menuService.menuList$;
+
+  /** 마우스를 올려서 활성화된 메뉴 ID */
+  activeMenuId?: number;
+
+  /** 현재 메뉴 URL */
+  activeMenuUrl?: string;
 
   ngOnInit(): void {
     this.menuList$.subscribe((menuList) => {
@@ -35,6 +50,11 @@ export class LayoutMenuComponent implements OnInit {
   /** 메뉴 목록을 조회한다. */
   listMenu(): void {
     this.menuService.listMenu();
+  }
+
+  /** 1뎁스 메뉴에 마우스를 올려서 펼치고 접는다. */
+  toggleSubMenu(menuId: number, isMouseEnter: boolean): void {
+    this.activeMenuId = isMouseEnter ? menuId : null;
   }
 
 }
