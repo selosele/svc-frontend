1692b3d1dc0d7f157b7830f727a7b5e4ec8bc5ea
selosele <osh3384@naver.com>
2024-06-23 18:33:12 +0900

    링크 컴포넌트 개선

--- src/app/shared/components/ui/ui-link/ui-link.component.ts
+++ src/app/shared/components/ui/ui-link/ui-link.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
+import { Params, RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -15,5 +15,16 @@ export class UiLinkComponent {
 
   /** router link */
   @Input() routerLink?: string;
+
+  /** router queryParams */
+  @Input() queryParams?: Params | null;
+
+  /** 버튼 클릭 이벤트 */
+  @Output() click? = new EventEmitter<Event>();
+
+  /** 버튼을 클릭한다. */
+  onClick(event: Event): void {
+    this.click.emit(event);
+  }
 
 }
