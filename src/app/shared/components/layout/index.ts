5133bf28d1a704a3ceb5bf7180b9397b0be3262c
selosele <osh3384@naver.com>
2024-06-30 16:36:41 +0900

    페이지 설명 컴포넌트 개발

--- src/app/shared/components/layout/index.ts
+++ src/app/shared/components/layout/index.ts
@@ -1,3 +1,4 @@
 export * from './layout-header/layout-header.component';
 export * from './layout-menu/layout-menu.component';
+export * from './layout-page-description/layout-page-description.component';
 export * from './layout-site-title/layout-site-title.component';
---
+++ src/app/shared/components/layout/layout-page-description/layout-page-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'layout-page-description',
+  templateUrl: './layout-page-description.component.html',
+  styleUrl: './layout-page-description.component.scss',
+  encapsulation: ViewEncapsulation.None,
+})
+export class LayoutPageDescriptionComponent {
+
+  
+
+}
