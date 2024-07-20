12dbd5920b9ea45ab6f05d9aed47b568bd689c9a
selosele <osh3384@naver.com>
2024-07-20 12:16:58 +0900

    리팩토링

--- src/app/human/human-mypage/human-mypage.component.ts
+++ src/app/human/human-mypage/human-mypage.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { LayoutPageDescriptionComponent } from '@app/shared/components';
 
 @Component({
   standalone: true,
-  imports: [
-    LayoutPageDescriptionComponent,
-  ],
-  selector: 'view-human-mypage',
+  imports: [],
+  selector: 'modal-human-mypage',
   templateUrl: './human-mypage.component.html',
   styleUrl: './human-mypage.component.scss'
 })
