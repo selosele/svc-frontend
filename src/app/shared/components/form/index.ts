04845f0a24e166dcba4f1c414c240f51d2a21816
selosele <osh3384@naver.com>
2024-07-21 17:51:29 +0900

    split form 컴포넌트 개발

--- src/app/shared/components/form/index.ts
+++ src/app/shared/components/form/index.ts
@@ -4,4 +4,5 @@ export * from './ui-checkbox/ui-checkbox.component';
 export * from './ui-checkbox-group/ui-checkbox-group.component';
 export * from './ui-dropdown/ui-dropdown.component';
 export * from './ui-form/ui-form.component';
+export * from './ui-split-form/ui-split-form.component';
 export * from './ui-text-field/ui-text-field.component';
---
+++ src/app/shared/components/form/ui-split-form/ui-split-form.component.ts
@@ -0,0 +1,40 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UiFormComponent } from '../ui-form/ui-form.component';
+import { UiButtonComponent } from '../../ui';
+
+@Component({
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    UiButtonComponent,
+  ],
+  selector: 'ui-split-form',
+  templateUrl: './ui-split-form.component.html',
+  styleUrl: './ui-split-form.component.scss'
+})
+export class UiSplitFormComponent extends UiFormComponent {
+
+  /** 저장 버튼 사용 여부 */
+  @Input() useSubmit = true;
+
+  /** 삭제 버튼 사용 여부 */
+  @Input() useRemove = false;
+
+  /** 삭제 버튼 클릭 이벤트 */
+  @Output() remove = new EventEmitter<Event>();
+
+  /** 닫기 버튼 클릭 이벤트 */
+  @Output() close = new EventEmitter<Event>();
+
+  /** 삭제 버튼을 클릭한다. */
+  protected onRemove(event: Event): void {
+    this.remove.emit(event);
+  }
+
+  /** 닫기 버튼을 클릭해서 splitter를 비활성화한다. */
+  protected onClose(event: Event): void {
+    this.close.emit(event);
+  }
+
+}
