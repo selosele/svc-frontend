ae70daccca62a6345d65b94b1fb912de6d6ee469
selosele <osh3384@naver.com>
2024-07-27 17:05:04 +0900

    트리테이블 컴포넌트 분리

--- src/app/shared/components/ui/index.ts
+++ src/app/shared/components/ui/index.ts
@@ -8,3 +8,4 @@ export * from './ui-skeleton/ui-skeleton.component';
 export * from './ui-splitter/ui-splitter.component';
 export * from './ui-table/ui-table.component';
 export * from './ui-table-buttons/ui-table-buttons.component';
+export * from './ui-tree-table/ui-tree-table.component';
---
+++ src/app/shared/components/ui/ui-tree-table/ui-tree-table.component.ts
@@ -0,0 +1,134 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { TreeTable, TreeTableModule, TreeTableNodeExpandEvent, TreeTableNodeUnSelectEvent } from 'primeng/treetable';
+import { TreeNode, TreeTableNode } from 'primeng/api';
+import { deepCopy } from '@app/shared/utils';
+import { Column } from '@app/shared/models';
+import { UiTableButtonsComponent } from '../ui-table-buttons/ui-table-buttons.component';
+
+@Component({
+  standalone: true,
+  imports: [
+    CommonModule,
+    TreeTableModule,
+    UiTableButtonsComponent,
+  ],
+  selector: 'ui-tree-table',
+  templateUrl: './ui-tree-table.component.html',
+  styleUrl: './ui-tree-table.component.scss'
+})
+export class UiTreeTableComponent implements OnInit {
+
+  /** 트리테이블 */
+  @ViewChild('treeTable') treeTable: TreeTable;
+
+  /** 트리테이블 행 추가 버튼 사용여부 */
+  @Input() useAdd = true;
+
+  /** 트리테이블 행 삭제 버튼 사용여부 */
+  @Input() useRemove = true;
+
+  /** 트리테이블 행 선택 모드 */
+  @Input() selectionMode: 'single' | 'multiple' = 'single';
+
+  /** 트리테이블 스크롤 높이 값 */
+  @Input() scrollHeight = '400px';
+
+  /** 트리테이블 컬럼 목록 */
+  @Input() cols!: Column[];
+
+  /** 트리테이블 데이터 */
+  @Input() data?: any;
+
+  /** 트리테이블 행 선택 key */
+  @Input() dataKey?: any;
+
+  /** 트리테이블 로딩 */
+  @Input() loading?: boolean = false;
+
+  /** 트리테이블 정렬 모드 */
+  @Input() sortMode?: 'single' | 'multiple' = 'single';
+
+  /** 트리테이블 선택된 행 */
+  @Input() selection?: TreeNode;
+
+  /** 트리테이블 새로고침 이벤트 */
+  @Output() refresh = new EventEmitter<Event>();
+
+  /** 트리테이블 선택 이벤트 */
+  @Output() nodeSelect = new EventEmitter<TreeTableNode>();
+
+  /** 트리테이블 선택 해제 이벤트 */
+  @Output() nodeUnSelect = new EventEmitter<TreeTableNodeUnSelectEvent>();
+
+  /** 트리테이블 행 그룹 펼치기 이벤트 */
+  @Output() nodeExpand = new EventEmitter<TreeTableNodeUnSelectEvent>();
+
+  /** 트리테이블 데이터 초기 값 */
+  private initialValue = [];
+
+  /** 트리테이블 행 정렬 여부 */
+  private isSorted: boolean | null = null;
+
+  ngOnInit(): void {
+    this.initialValue = deepCopy(this.data);
+  }
+
+  /** 트리테이블 새로고침 버튼을 클릭한다. */
+  protected onRefresh(event: Event): void {
+    this.refresh.emit(event);
+  }
+
+  /** 트리테이블 행을 선택한다. */
+  protected onNodeSelect(event: TreeTableNode): void {
+    this.nodeSelect.emit(event);
+  }
+
+  /** 트리테이블 행을 선택 해제한다. */
+  protected onNodeUnselect(event: TreeTableNodeUnSelectEvent): void {
+    this.nodeUnSelect.emit(event);
+  }
+
+  /** 트리테이블 행 그룹을 펼친다. */
+  protected onNodeExpand(event: TreeTableNodeExpandEvent): void {
+    this.nodeExpand.emit(event);
+  }
+
+  /** 트리테이블 행 그룹 펼치기 버튼을 클릭한다. */
+  protected onTogglerClick(event: Event): void {
+    event.stopPropagation(); // 펼치기 버튼 클릭 시, 행선택 이벤트가 동시에 발생하는 이슈를 방지
+  }
+
+  /** 트리테이블 행 정렬 커스텀 이벤트 */
+  protected onSort(event: any): void {
+    if (this.isSorted == null || this.isSorted === undefined) {
+      this.isSorted = true;
+      this.sortTreeTableData(event);
+    } else if (this.isSorted == true) {
+      this.isSorted = false;
+      this.sortTreeTableData(event);
+    } else if (this.isSorted == false) {
+      this.isSorted = null;
+      this.data = [...this.initialValue];
+      this.treeTable.reset();
+    }
+  }
+  
+  /** 트리테이블 행을 정렬한다. */
+  private sortTreeTableData(event: any): void {
+    event.data?.sort((data1, data2) => {
+      const value1 = data1.data[event.field];
+      const value2 = data2.data[event.field];
+      let result = null;
+
+      if (value1 == null && value2 != null) result = -1;
+      else if (value1 != null && value2 == null) result = 1;
+      else if (value1 == null && value2 == null) result = 0;
+      else if (typeof value1 === 'string' && typeof value2 === 'string') result = value1.localeCompare(value2);
+      else result = (value1 < value2) ? -1 : (value1 > value2) ? 1 : 0;
+
+      return event.order * result;
+    });
+  }
+
+}
