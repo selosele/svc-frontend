1da5b19c6ae72b098bee3689f53603be58e0144e
selosele <osh3384@naver.com>
2024-08-04 16:33:39 +0900

    권한관리 상세 모달 개발

---
+++ src/app/system/system-role/system-role-detail/system-role-detail.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { UserResponseDTO } from '@app/auth/auth.model';
+import { MenuTree } from '@app/menu/menu.model';
+import { LayoutPageDescriptionComponent, UiTableComponent, UiTreeTableComponent } from '@app/shared/components';
+import { DynamicDialogConfig } from 'primeng/dynamicdialog';
+
+@Component({
+  standalone: true,
+  imports: [
+    LayoutPageDescriptionComponent,
+    UiTableComponent,
+    UiTreeTableComponent,
+  ],
+  selector: 'modal-system-role-detail',
+  templateUrl: './system-role-detail.component.html',
+  styleUrl: './system-role-detail.component.scss'
+})
+export class SystemRoleDetailComponent implements OnInit {
+
+  constructor(
+    private config: DynamicDialogConfig,
+  ) {}
+
+  /** 사용자 목록 */
+  userList: UserResponseDTO[];
+
+  /** 메뉴 트리 목록 */
+  menuTree: MenuTree[];
+
+  /** 사용자 목록 테이블 컬럼 */
+  userListcols = [
+    { field: 'userId',        header: '사용자 ID' },
+    { field: 'userAccount',   header: '사용자 계정' },
+    { field: 'employeeName',  header: '직원명' },
+    { field: 'companyName',   header: '회사명' },
+    { field: 'userActiveYn',  header: '사용자 활성화 여부' },
+    { field: 'rolesString',   header: '권한' },
+  ];
+
+  /** 메뉴 트리 목록 테이블 컬럼 */
+  menuTreecols = [
+    { field: 'menuId',        header: '메뉴 ID' },
+    { field: 'menuName',      header: '메뉴명' },
+    { field: 'menuUrl',       header: '메뉴 URL' },
+    { field: 'menuDepth',     header: '메뉴 뎁스' },
+    { field: 'useYn',         header: '사용여부' },
+  ];
+
+  ngOnInit(): void {
+    this.userList = this.config.data['userList'];
+    this.menuTree = this.config.data['menuTree'];
+  }
+
+}
--- src/app/system/system-role/system-role.component.ts
+++ src/app/system/system-role/system-role.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { LayoutPageDescriptionComponent, UiSkeletonComponent, UiTableComponent } from '@app/shared/components';
 import { AuthService } from '@app/auth/auth.service';
 import { RoleResponseDTO } from '@app/auth/auth.model';
+import { UiDialogService } from '@app/shared/services';
+import { MenuService } from '@app/menu/menu.service';
+import { SystemRoleDetailComponent } from './system-role-detail/system-role-detail.component';
+import { combineLatest } from 'rxjs';
 
 @Component({
   standalone: true,
@@ -17,7 +21,9 @@ import { RoleResponseDTO } from '@app/auth/auth.model';
 export class SystemRoleComponent implements OnInit {
 
   constructor(
+    private dialogService: UiDialogService,
     private authService: AuthService,
+    private menuService: MenuService,
   ) {}
 
   /** 권한 목록 */
@@ -33,11 +39,11 @@ export class SystemRoleComponent implements OnInit {
   /** 테이블 선택된 행 */
   selection: RoleResponseDTO;
 
-  /** 테이블 컬럼 목록 */
+  /** 테이블 컬럼 */
   cols = [
-    { field: 'roleId',    header: '권한 ID'},
-    { field: 'roleName',  header: '권한명'},
-    { field: 'roleOrder', header: '권한 순서'},
+    { field: 'roleId',    header: '권한 ID' },
+    { field: 'roleName',  header: '권한명' },
+    { field: 'roleOrder', header: '권한 순서' },
   ];
 
   ngOnInit(): void {
@@ -49,6 +55,22 @@ export class SystemRoleComponent implements OnInit {
   /** 권한 목록을 조회한다. */
   listRole(): void {
     this.authService.listRole();
+  }
+
+  /** 테이블 행을 선택한다. */
+  onRowSelect(event: any): void {
+    const listUserByRole$ = this.authService.listUserByRole({ roleIdList: [event.data['roleId']] });
+    const listMenuByRole$ = this.menuService.listMenuByRole({ roleIdList: [event.data['roleId']] });
+
+    combineLatest([listUserByRole$, listMenuByRole$]).subscribe(([userList, menuList]) => {
+      const menuTree = this.menuService.createMenuTree(menuList);
+
+      this.dialogService.open(SystemRoleDetailComponent, {
+        focusOnShow: false,
+        header: `"${event.data['roleName']}" 권한별 사용자 및 메뉴 목록 조회`,
+        data: { userList, menuTree },
+      });
+    });
   }
   
   /** 테이블 새로고침 버튼을 클릭한다. */
