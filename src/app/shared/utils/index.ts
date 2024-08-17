42c2147e74e53be67f3f28068783af53c07da451
selosele <osh3384@naver.com>
2024-08-17 15:13:15 +0900

    dayjs 공통 유틸 개발

--- src/app/shared/utils/index.ts
+++ src/app/shared/utils/index.ts
@@ -1,8 +1,9 @@
-// 공통
-export * from './global/lang.util';
-
 // 인증
 export * from './auth/auth.util';
 
 // form
 export * from './form/validation.util';
+
+// 공통
+export * from './global/date.util';
+export * from './global/lang.util';
