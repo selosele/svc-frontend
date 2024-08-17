ade272031f5588264fd2b748851906074d16d2b7
selosele <osh3384@naver.com>
2024-08-17 11:45:18 +0900

    일부 페이지 URL 변경

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { AppComponent } from './app.component';
 import { authRoutes } from './auth/auth.routes';
 import { indexRoutes } from './index/index.routes';
 import { humanRoutes } from './human/human.routes';
-import { scheduleRoutes } from './schedule/schedule.routes';
 import { systemRoutes } from './system/system.routes';
 
 /** 공통 라우터 */
@@ -27,7 +26,6 @@ export const routes: Routes = [
   ...authRoutes,     // 인증 페이지
   ...indexRoutes,    // 메인 페이지
   ...humanRoutes,    // 인사관리 페이지
-  ...scheduleRoutes, // 일정관리 페이지
   ...systemRoutes,   // 시스템관리 페이지
   ...globalRoutes,   // 공통
 ];
--- src/app/human/human.routes.ts
+++ src/app/human/human.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
+import { roles } from '@app/shared/utils';
+import { authGuard, menuGuard } from '@app/shared/guards';
 
 /** 인사관리 페이지 라우터 */
 export const humanRoutes: Routes = [
   
-
+  // 일정관리 페이지
+  {
+    path: 'human/schedules',
+    canActivate: [authGuard, menuGuard],
+    data: {
+      roles: [roles.EMPLOYEE],
+    },
+    loadComponent: () => import('./schedule/schedule.component').then(x => x.ScheduleComponent),
+  },
   
 ];
