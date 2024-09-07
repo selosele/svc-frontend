50455c9172eb61538ce6550037ac3782cb0256d5
selosele <osh3384@naver.com>
2024-09-07 17:17:04 +0900

    코드 리졸버 적용 및 선택휴가계산 기능 개발

--- src/app/human/human-vacation/human-vacation.component.ts
+++ src/app/human/human-vacation/human-vacation.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { LayoutPageDescriptionComponent } from '@app/shared/components/layout';
-import { UiSkeletonComponent, UiTabComponent } from '@app/shared/components/ui';
-import { Tab, UiTabChangeEvent } from '@app/shared/models';
+import { UiCardComponent, UiSkeletonComponent, UiTabComponent } from '@app/shared/components/ui';
 import { AuthenticatedUser } from '@app/auth/auth.model';
 import { AuthService } from '@app/auth/auth.service';
 import { HumanService } from '../human.service';
 import { HumanVacationListComponent } from './human-vacation-list/human-vacation-list.component';
+import { DropdownData } from '@app/shared/components/form/ui-dropdown/ui-dropdown.model';
+import { UiFormComponent } from '@app/shared/components/form/ui-form/ui-form.component';
+import { UiCheckboxComponent } from '@app/shared/components/form/ui-checkbox/ui-checkbox.component';
+import { UiCheckboxGroupComponent } from '@app/shared/components/form/ui-checkbox-group/ui-checkbox-group.component';
+import { Tab, UiTabChangeEvent } from '@app/shared/components/ui/ui-tab/ui-tab.model';
 
 @Component({
   standalone: true,
   imports: [
     RouterModule,
+    ReactiveFormsModule,
     UiSkeletonComponent,
     UiTabComponent,
+    UiFormComponent,
+    UiCheckboxGroupComponent,
+    UiCheckboxComponent,
+    UiCardComponent,
     LayoutPageDescriptionComponent,
     HumanVacationListComponent,
   ],
@@ -24,6 +34,8 @@ import { HumanVacationListComponent } from './human-vacation-list/human-vacation
 export class HumanVacationComponent implements OnInit {
 
   constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
     private authService: AuthService,
     private humanService: HumanService,
   ) {}
@@ -36,6 +48,12 @@ export class HumanVacationComponent implements OnInit {
 
   /** 선택된 회사 탭 index */
   activeIndex: number;
+
+  /** 휴가 계산 폼 */
+  caculateVacationForm: FormGroup;
+
+  /** 휴가 구분 코드 데이터 목록 */
+  vacationTypeCodes: DropdownData[];
 
   /** 휴가 테이블 타이틀 */
   get vacationTableTitle() {
@@ -58,12 +76,20 @@ export class HumanVacationComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.route.data.subscribe(({ code }) => {
+      this.vacationTypeCodes = code['VACATION_TYPE_00'];
+    });
+
     this.user = this.authService.getAuthenticatedUser();
     this.humanService.setWorkHistoryId(parseInt(`${this.user.workHistoryId}`));
 
     if (!this.workHistoryListDataLoad) {
       this.listWorkHistory();
     }
+
+    this.caculateVacationForm = this.fb.group({
+      vacationTypeCode: [['01', '02', '03', '04', '05', '06', '12']], // 휴가 계산에 포함할 휴가 구분 코드
+    });
   }
 
   /** 근무이력 목록을 조회한다. */
@@ -76,6 +102,11 @@ export class HumanVacationComponent implements OnInit {
     this.activeIndex = event.index;
     this.humanService.setWorkHistoryId(event.activeKey);
     this.humanService.setVacationTableTitle(this.activeIndex);
+  }
+
+  /** 휴가를 계산한다. */
+  onSubmit(values): void {
+
   }
 
 }
