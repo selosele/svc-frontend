ae0cfe63ee411eb05bfb1da0f6e536f2c055a6a7
selosele <osh3384@naver.com>
2024-09-15 17:36:07 +0900

    에러페이지 UI 개선

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+import { UiButtonComponent } from '@app/shared/components/ui';
+import { StoreService } from '@app/shared/services';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [
+    UiButtonComponent,
+  ],
   selector: 'view-error',
   templateUrl: './error.component.html',
   styleUrl: './error.component.scss'
 })
 export class ErrorComponent {
+
+  constructor(
+    private router: Router,
+    private location: Location,
+    private store: StoreService,
+  ) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.store.update<string>('currentPageTitle', null);
+      });
+  }
+
+  /** 메인 페이지로 이동한다. */
+  goMainPage(): void {
+    this.router.navigateByUrl('/index');
+  }
+
+  /** 이전 페이지로 이동한다. */
+  goBackPage(): void {
+    this.location.back();
+  }
 
 }
