ed4b9a911714eed64ed6b209d4d8212a9c4577a2
selosele <osh3384@naver.com>
2024-09-15 17:41:29 +0900

    NavigationEnd 이벤트가 발생할때마다 메뉴데이터 설정을 하도록 개선

--- src/app/shared/guards/menu/menu.guard.ts
+++ src/app/shared/guards/menu/menu.guard.ts
@@ -1,5 +1,6 @@
 import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivateFn, NavigationEnd, Router, RouterStateSnapshot } from '@angular/router';
+import { filter } from 'rxjs';
 import { MenuResponseDTO } from '@app/menu/menu.model';
 import { MenuService } from '@app/menu/menu.service';
 import { StoreService } from '@app/shared/services';
@@ -7,35 +8,40 @@ import { isNotEmpty } from '@app/shared/utils';
 
 /** 메뉴 guard */
 export const menuGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  const router = inject(Router);
   const store = inject(StoreService);
   const menuService = inject(MenuService);
   const menuList$ = store.select<MenuResponseDTO[]>('menuList').asObservable();
 
-  menuList$.subscribe((menuList) => {
-    if (menuList.length === 0) return;
-
-    /** 메뉴 관련 데이터를 설정한다. */
-    menuService.setData();
-
-    /** 현재 메뉴 ID */
-    const menuId = parseInt(route.queryParams['menuId']);
-
-    /** 현재 메뉴 URL */
-    const menuUrl = `/${route.routeConfig.path}`;
-
-    /** 현재 메뉴명 */
-    const menuName = menuList.find(x => x.menuId === menuId)?.menuName;
-
-    /** 메뉴접속이력 데이터 */
-    const menuHistoryList = store.select<MenuResponseDTO[]>('menuHistoryList').value;
-
-    // 같은 메뉴접속이력 데이터가 존재하는지 확인한다.
-    const hasExist = isNotEmpty(menuHistoryList.find(x => x.menuId === menuId));
-    if (hasExist) return;
-
-    // 메뉴접속이력 데이터를 쌓아 넣는다.
-    menuService.setMenuHistoryList([...menuHistoryList, { menuId, menuUrl, menuName }]);
-  });
+  router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe(() => {
+      menuList$.subscribe((menuList) => {
+        if (menuList.length === 0) return;
+    
+        /** 메뉴 관련 데이터를 설정한다. */
+        menuService.setData();
+    
+        /** 현재 메뉴 ID */
+        const menuId = parseInt(route.queryParams['menuId']);
+    
+        /** 현재 메뉴 URL */
+        const menuUrl = `/${route.routeConfig.path}`;
+    
+        /** 현재 메뉴명 */
+        const menuName = menuList.find(x => x.menuId === menuId)?.menuName;
+    
+        /** 메뉴접속이력 데이터 */
+        const menuHistoryList = store.select<MenuResponseDTO[]>('menuHistoryList').value;
+    
+        // 같은 메뉴접속이력 데이터가 존재하는지 확인한다.
+        const hasExist = isNotEmpty(menuHistoryList.find(x => x.menuId === menuId));
+        if (hasExist) return;
+    
+        // 메뉴접속이력 데이터를 쌓아 넣는다.
+        menuService.setMenuHistoryList([...menuHistoryList, { menuId, menuUrl, menuName }]);
+      });
+    });
   
   return true;
 };
