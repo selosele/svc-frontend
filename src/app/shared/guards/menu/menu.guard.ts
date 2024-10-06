cc416e7d0ecd5f63e3a4096594318dd6ebb41e26
selosele <osh3384@naver.com>
2024-10-06 13:01:34 +0900

    메뉴 가드 개선

--- src/app/shared/guards/menu/menu.guard.ts
+++ src/app/shared/guards/menu/menu.guard.ts
@@ -12,11 +12,25 @@ export const menuGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: R
   const menuService = inject(MenuService);
   const menuList$ = store.select<MenuResponseDTO[]>('menuList').asObservable();
 
+  // menuId 파라미터가 없으면 에러페이지로 이동한다.
+  if (!route.queryParams.menuId) {
+    router.navigateByUrl('/error');
+    return false;
+  }
+
   router.events
     .pipe(filter(event => event instanceof NavigationEnd))
     .subscribe(() => {
       menuList$.subscribe((menuList) => {
         if (menuList.length === 0) return;
+
+        const currentMenu = menuList.find(menu => menu.menuId === Number(route.queryParams.menuId));
+
+        // 메뉴가 없거나 미사용 or 삭제된 메뉴일경우 에러페이지로 이동한다.
+        if (!currentMenu || currentMenu.useYn === 'N' || currentMenu.deleteYn === 'Y') {
+          router.navigateByUrl('/error');
+          return;
+        }
     
         // 메뉴 관련 데이터를 설정한다.
         menuService.setData();
