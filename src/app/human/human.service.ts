5cd3c94e6f23690abd70b3a406bf40a0d5a634db
selosele <osh3384@naver.com>
2024-10-19 14:12:28 +0900

    리팩토링

--- src/app/human/human.service.ts
+++ src/app/human/human.service.ts
@@ -172,7 +172,7 @@ export class HumanService {
         const nowDateDiff = nowDate.diff(dateUtil(joinYmd), 'month');
         const joinYmdFormat = dateUtil(joinYmd).format('YYYY년 MM월 DD일');
 
-        return `잔여 월차: <strong class="text-primary">${vacationRemainCount}</strong>/${nowDateDiff}개 (입사 ${joinYmdFormat}부터 총 ${nowDateDiff}개 월차가 발생하였음)`;
+        return `잔여 월차: <strong class="text-primary">${vacationRemainCount}</strong>/${nowDateDiff}개 (입사 ${joinYmdFormat}부터 총 ${nowDateDiff}개의 월차가 발생)`;
       // 회계년도 기준
       case 'FISCAL_YEAR':
         return ``;
