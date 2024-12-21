f4af0ffcbd736c43169ac88582b3909f81e78d22
selosele <osh3384@naver.com>
2024-12-21 17:35:00 +0900

    회사등록신청 상세조회 화면 개발

--- src/app/human/human-my-info/human-my-info-company-apply-detail/human-my-info-company-apply-detail.component.ts
+++ src/app/human/human-my-info/human-my-info-company-apply-detail/human-my-info-company-apply-detail.component.ts
@@ -5,10 +5,18 @@ import { HumanService } from '@app/human/human.service';
 import { CompanyApplyResponseDTO } from '@app/human/human.model';
 import { UiMessageService } from '@app/shared/services';
 import { isObjectEmpty } from '@app/shared/utils';
+import { FormValidator, UiHiddenFieldComponent, UiSplitFormComponent, UiTextareaComponent, UiTextFieldComponent } from '@app/shared/components/form';
+import { UiContentTitleComponent } from '@app/shared/components/ui';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [
+    UiSplitFormComponent,
+    UiTextFieldComponent,
+    UiTextareaComponent,
+    UiHiddenFieldComponent,
+    UiContentTitleComponent,
+  ],
   selector: 'human-my-info-company-apply-detail',
   templateUrl: './human-my-info-company-apply-detail.component.html',
   styleUrl: './human-my-info-company-apply-detail.component.scss'
@@ -29,7 +37,7 @@ export class HumanMyInfoCompanyApplyDetailComponent implements OnInit, OnChanges
   detailForm: FormGroup;
 
   /** 삭제 버튼 사용 여부 */
-  useRemove = true;
+  useRemove = false;
 
   /** 데이터 새로고침 이벤트 */
   @Output() refresh = new EventEmitter<void>();
@@ -41,21 +49,33 @@ export class HumanMyInfoCompanyApplyDetailComponent implements OnInit, OnChanges
   @Output() close = new EventEmitter<void>();
 
   ngOnInit(): void {
-    
+    this.detailForm = this.fb.group({
+      companyApplyId: [''],                           // 회사등록신청 ID
+      registrationNo: [''],                           // 사업자등록번호
+      corporateName: [''],                            // 법인명
+      companyName: ['', [FormValidator.required]],    // 회사명
+      applyContent: [''],                             // 신청 내용
+      applyStateCode: [''],                           // 신청 상태 코드
+      applyStateCodeName: [''],                       // 신청 상태 코드명
+      applyDt: [''],                                  // 신청일시
+    });
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.detail && this.detailForm) {
-      this.useRemove = true;
       this.detailForm.reset();
 
       if (isObjectEmpty(changes.detail.currentValue)) {
-        this.useRemove = false;
         return;
       }
       
       this.detailForm.patchValue(this.detail);
     }
+  }
+
+  /** 회사등록신청정보를 저장한다. */
+  onSubmit(value): void {
+
   }
 
   /** 닫기 버튼을 클릭한다. */
