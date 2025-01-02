8c73fe2b2b400c7054f8e13fd9ec29b57c030a32
selosele <osh3384@naver.com>
2025-01-02 11:02:37 +0900

    파일명 패턴 수정

--- src/app/shared/services/index.ts
+++ src/app/shared/services/index.ts
@@ -5,6 +5,6 @@ export * from './http/http.service';
 export * from './store/store.service';
 
 // ui
-export * from './ui/ui-dialog-service';
-export * from './ui/ui-loading-service';
-export * from './ui/ui-message-service';
+export * from './ui/ui-dialog.service';
+export * from './ui/ui-loading.service';
+export * from './ui/ui-message.service';
