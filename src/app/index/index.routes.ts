cb34c826cde6d4b19d78499b6aaebba04be4fcf3
selosele <osh3384@naver.com>
2025-01-09 15:06:38 +0900

    메뉴가드 오작동 이슈 수정

--- src/app/index/index.routes.ts
+++ src/app/index/index.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { authGuard } from '@app/shared/guards';
+import { authGuard, menuGuard } from '@app/shared/guards';
 
 /** 메인 라우터 */
 export const indexRoutes: Routes = [
@@ -7,7 +7,7 @@ export const indexRoutes: Routes = [
   // 메인 페이지
   {
     path: 'index',
-    canActivate: [authGuard],
+    canActivate: [authGuard, menuGuard],
     loadComponent: () => import('./index.component').then(x => x.IndexComponent),
   },
 ];
--- src/app/shared/guards/menu/menu.guard.ts
+++ src/app/shared/guards/menu/menu.guard.ts
@@ -1,44 +1,59 @@
 import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateFn, NavigationEnd, Router, RouterStateSnapshot } from '@angular/router';
-import { filter } from 'rxjs';
-import { MenuResponseDTO } from '@app/menu/menu.model';
-import { MenuService } from '@app/menu/menu.service';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
+import { filter, firstValueFrom, take } from 'rxjs';
 import { StoreService } from '@app/shared/services';
+import { MenuService } from '@app/menu/menu.service';
+import { MenuResponseDTO } from '@app/menu/menu.model';
 
 /** 메뉴 guard */
-export const menuGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+export const menuGuard: CanActivateFn = async (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
   const router = inject(Router);
   const store = inject(StoreService);
   const menuService = inject(MenuService);
-  const menuList$ = store.select<MenuResponseDTO[]>('menuList').asObservable();
 
-  // menuId 파라미터가 없으면 에러페이지로 이동한다.
-  if (!route.queryParams.menuId) {
-    router.navigateByUrl('/error');
+  // 메뉴 목록을 조회한다.
+  if (!store.select<boolean>('menuListDataLoad').value) {
+    menuService.listMenu();
+  }
+  
+  const menuList = await firstValueFrom(
+    store.select<MenuResponseDTO[]>('menuList').pipe(
+      filter(menuList => Array.isArray(menuList) && menuList.length > 0),
+      take(1)
+    )
+  );
+
+  // 메뉴 목록이 없으면 에러페이지로 이동한다.
+  if (!menuList || menuList?.length === 0) {
+    await router.navigateByUrl('/error');
     return false;
   }
 
-  router.events
-    .pipe(filter(event => event instanceof NavigationEnd))
-    .subscribe(() => {
-      menuList$.subscribe((menuList) => {
-        if (menuList.length === 0) return;
+  // menuId 파라미터가 없으면 에러페이지로 이동한다.
+  if (isExcludePath(route.routeConfig.path) && !route.queryParams.menuId) {
+    await router.navigateByUrl('/error');
+    return false;
+  }
 
-        const currentMenu = menuList.find(menu => menu.menuId === Number(route.queryParams.menuId));
+  // 현재 메뉴를 찾는다.
+  const currentMenu = menuList.find(menu => menu.menuId === Number(route.queryParams.menuId));
 
-        // 미사용 or 삭제된 메뉴일경우 에러페이지로 이동한다.
-        // 2025.01.06. 로그아웃 후 다른 유저로 재로그인하는 즉시 menuGuard를 타면서
-        // !currentMenu 조건을 만족하게 되어 에러 페이지로 이동되는 현상 수정을 위한 주석 처리
-//      if (!currentMenu || currentMenu.useYn === 'N' || currentMenu.deleteYn === 'Y') {
-        if (currentMenu?.useYn === 'N' || currentMenu?.deleteYn === 'Y') {
-          router.navigateByUrl('/error');
-          return;
-        }
+  if (isExcludePath(route.routeConfig.path)) {
     
-        // 메뉴 관련 데이터를 설정한다.
-        menuService.setData();
-      });
-    });
-  
+    // 미사용 or 삭제된 메뉴일경우 에러페이지로 이동한다.
+    if (!currentMenu || currentMenu.useYn === 'N' || currentMenu.deleteYn === 'Y') {
+      await router.navigateByUrl('/error');
+      return false;
+    }
+  }
+
+  // 메뉴 관련 데이터를 설정한다.
+  menuService.setData();
   return true;
 };
+
+/** 제외 경로 여부를 반환한다. */
+function isExcludePath(path: string): boolean {
+  const excludeUrls = ['/', 'index', 'auth/login'];
+  return !excludeUrls.includes(path);
+}
