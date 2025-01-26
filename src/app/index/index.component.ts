688739cca5c078a8daf2323dc694281996654671
selosele <osh3384@naver.com>
2025-01-26 19:31:19 +0900

    메인화면 > 휴가통계 UI 개발

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@app/auth/auth.service';
+import { HumanService } from '@app/human/human.service';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [
+
+  ],
   selector: 'view-index',
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit {
+
+  constructor(
+    private authService: AuthService,
+    private humanService: HumanService,
+  ) {}
+
+  /** 인증된 사용자 정보 */
+  get user() {
+    return this.authService.getAuthenticatedUser();
+  }
+
+  ngOnInit() {
+    this.humanService.listVacationStats$({ userId: this.user?.userId })
+    .subscribe((data) => {
+
+    });
+  }
 
 }
